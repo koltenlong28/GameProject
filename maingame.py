a150688ccbd8a0c2dfec3884379c646f2add6b66
koltenlong28 <145070205+koltenlong28@users.noreply.github.com>
2023-12-12 15:23:32 -0800

    Create maingame.py

---
+++ maingame.py
@@ -0,0 +1,19 @@
+import turtle as trtl
+import random
+
+leaderboard_file_name = "1LeaderBoard.dat"
+leader_names_list = []
+leader_scores_list = []
+player_name = input("Please enter your name:")
+player_password = input("please enter your password:")
+
+
+
+player_password = False
+if not player_password:
+     print("incorrectpassword")
+
+
+
+
+
